11a1fec088d8e8777a3420262e23b4273cedfdcb
Christoph DeMaskey <christoph.demaskey@gmail.com>
2024-02-21 00:28:24 -0500

    Finish implementing the primary calculations for the Warsaw Approach.

--- src/components/WarsawApproach.vue
+++ src/components/WarsawApproach.vue
@@ -9,7 +9,12 @@ const warsawApproach = reactive({
     protien: 0,
     fatKCal:0,
     protienKCal:0,
-    totalKCal: 0
+    totalKCal: 0,
+    fatProtienUnits: 0,
+    fpuRounded: 0,
+    extendedBolusDuration: "-",
+    carbsPerFPU: 10,
+    extendedBolusInsulin: 0
 })
 
 watch(() => warsawApproach.fat, () => {
@@ -23,6 +28,21 @@ watch(() => warsawApproach.fatKCal, () => {
 })
 watch(()=> warsawApproach.protienKCal, () => {
     warsawApproach.totalKCal = warsawApproach.fatKCal + warsawApproach.protienKCal
+})
+watch(()=> warsawApproach.totalKCal, () => {
+    warsawApproach.fatProtienUnits = warsawApproach.totalKCal / 100
+})
+watch(() => warsawApproach.fatProtienUnits, () => {
+    warsawApproach.fpuRounded = Math.round(warsawApproach.fatProtienUnits)
+})
+watch(()=> warsawApproach.fpuRounded, () => {
+    if(warsawApproach.fpuRounded == 1) warsawApproach.extendedBolusDuration = "3 hours"
+    else if(warsawApproach.fpuRounded == 2) warsawApproach.extendedBolusDuration = "4 hours"
+    else if(warsawApproach.fpuRounded == 3) warsawApproach.extendedBolusDuration = "5 hours"
+    else if(warsawApproach.fpuRounded == 4) warsawApproach.extendedBolusDuration = "8 hours"
+})
+watch(() => warsawApproach.fatProtienUnits, () => {
+    warsawApproach.extendedBolusInsulin = warsawApproach.fatProtienUnits * warsawApproach.carbsPerFPU / warsawApproach.carbRatio
 })
 </script>
 
@@ -40,5 +60,11 @@ watch(()=> warsawApproach.protienKCal, () => {
     <div>Fat (kcal): {{ warsawApproach.fatKCal }}</div>
     <div>Protien (kcal): {{ warsawApproach.protienKCal }}</div>
     <div>Total (kcal): {{ warsawApproach.totalKCal }}</div>
+    <div>Fat-Protien Units (FPU): {{ warsawApproach.fatProtienUnits }}</div>
+    <div>FPU Rounded: {{ warsawApproach.fpuRounded }}</div>
+    <div>Duration of Extended Bolus: {{ warsawApproach.extendedBolusDuration }}</div>
+    <div>Carbs per FPU (g)</div>
+    <input v-model.number="warsawApproach.carbsPerFPU">
+    <div>Extended Bolus (u): {{ warsawApproach.extendedBolusInsulin }}</div>
 
 </template>
